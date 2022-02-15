aadc0e79a61e5b67d75dfb27bfedfac82d0d4ffb
Alex <lebeichuk.sasha@gmail.com>
2022-02-15 20:59:14 +0200

    - added button for adding new node  with a value from input from blazor

--- wwwroot/js/litegraphInit.js
+++ wwwroot/js/litegraphInit.js
@@ -3,10 +3,10 @@
 var canvas = new LGraphCanvas("#mycanvas", graph);
 var editor = new Editor(canvas, graph);
 
-var node_const1 = LiteGraph.createNode("basic/const");
-node_const1.pos = [100, 100];
-graph.add(node_const1);
-node_const1.setValue(4.5);
+// var node_const1 = LiteGraph.createNode("basic/const");
+// node_const1.pos = [100, 100];
+// graph.add(node_const1);
+// node_const1.setValue(4.5);
 
 var node_const2 = LiteGraph.createNode("basic/const");
 node_const2.pos = [100, 400];
@@ -21,8 +21,15 @@ var node_watch = LiteGraph.createNode("basic/watch");
 node_watch.pos = [800, 200];
 graph.add(node_watch);
 
-node_const1.connect(0,node_multiply,0);
+// node_const1.connect(0,node_multiply,0);
 node_const2.connect(0,node_multiply,1);
 node_multiply.connect(0, node_watch);
 
 graph.start()
+
+function SetConstantNode(nodeName,value){
+    var nodeName = LiteGraph.createNode("basic/const");
+    nodeName.pos = [100, 100];
+    graph.add(nodeName);
+    nodeName.setValue(value);
+}
