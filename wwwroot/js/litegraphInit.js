f135c22eff29f6eca207888e1ec20061f0415e3b
Alex <lebeichuk.sasha@gmail.com>
2022-02-16 15:37:12 +0200

    - removed default blazor pages - changed layout

--- wwwroot/js/litegraphInit.js
+++ wwwroot/js/litegraphInit.js
@@ -3,11 +3,6 @@
 var canvas = new LGraphCanvas("#mycanvas", graph);
 var editor = new Editor(canvas, graph);
 
-// var node_const1 = LiteGraph.createNode("basic/const");
-// node_const1.pos = [100, 100];
-// graph.add(node_const1);
-// node_const1.setValue(4.5);
-
 var node_const2 = LiteGraph.createNode("basic/const");
 node_const2.pos = [100, 400];
 graph.add(node_const2);
@@ -21,15 +16,14 @@ var node_watch = LiteGraph.createNode("basic/watch");
 node_watch.pos = [800, 200];
 graph.add(node_watch);
 
-// node_const1.connect(0,node_multiply,0);
 node_const2.connect(0,node_multiply,1);
 node_multiply.connect(0, node_watch);
 
 graph.start()
 
-function SetConstantNode(nodeName,value){
+function SetConstantNode(nodeName,value, positionX, positionY){
     var nodeName = LiteGraph.createNode("basic/const");
-    nodeName.pos = [100, 100];
+    nodeName.pos = [positionX, positionY];
     graph.add(nodeName);
     nodeName.setValue(value);
 }
