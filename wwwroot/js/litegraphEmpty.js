c99f6a6ab7e34c53db7ec2852cd9b0cefac5937d
Alex <lebeichuk.sasha@gmail.com>
2022-02-22 15:50:45 +0200

    - fix logic of running scripts
    - added disposable timer to check each second if graph changed

--- wwwroot/js/litegraphEmpty.js
+++ wwwroot/js/litegraphEmpty.js
@@ -3,4 +3,7 @@ var graph = new LGraph();
 var canvas = new LGraphCanvas("#mycanvas", graph);
 var editor = new Editor(canvas, graph);
 
+var graphNodesAtLoad = graph.serialize().nodes;
+var graphLinksAtLoad = graph.serialize().links; 
+
 graph.start()
